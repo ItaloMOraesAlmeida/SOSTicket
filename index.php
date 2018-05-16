396da9be75988ce774b30ff217db228debe72ac1
Italo <italomoraesalmeida@hotmail.com>
2018-05-15 21:45:44 -0300

    Verção 1.0

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<?php
+// Verificação do formulário de instalação!
+
+$filename = 'view/form.install.php';
+if (file_exists($filename)) {
+    header('location: '.$filename);
+    die;
+} else {
+
+?>
+
+<!-- Formulário de Login -->
+
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOSTicket</title>
+    
+</head>
+<body>
+    ok
+</body>
+</html>
+
+<!-- Fim do formulário -->
+
+<?php
+
+}
+
+?>
