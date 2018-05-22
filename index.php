da3f9a7d815ab221c1c0e4e986ecdb41a095af06
Italo <italomoraesalmeida@hotmail.com>
2018-05-21 23:22:42 -0300

    Alterações da noite, Atualizar todas as versões dos documentos

--- index.php
+++ index.php
@@ -18,10 +18,61 @@ if (file_exists($filename)) {
     <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SOSTicket</title>
-    
+
+    <!-- RESOUCERS -->
+    <link href="resources/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="resoucers/js/bootstrap.min.js"></script>
+    <script src="resoucers/js/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="resources/css/login.css">
 </head>
 <body>
-    Login
+    <section style="height: 100vh;">
+        <div style="background-attachment: fixed; background-size: cover; width: 100%; height: 100%; position: fixed;"  >
+            <div class="main">
+                <form action="login" method="post">
+                    <h1><span>SOSTicket</span></h1>
+                    <div class="login-wrap">
+                        <div class="login-html">
+                            <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label>
+                            <input id="tab-2" type="radio" name="tab" class="for-pwd"><label for="tab-2" class="tab">Recuperar Senha</label>
+                            <div class="login-form">
+                                <div class="sign-in-htm">
+                                    <div class="group">
+                                        <label for="user" class="label">Usuário</label>
+                                        <input id="user" type="text" class="input">
+                                    </div>
+                                    <div class="group">
+                                        <label for="pass" class="label">Senha</label>
+                                        <input id="pass" type="password" class="input" data-type="password">
+                                    </div>
+                                    <div class="group">
+                                        <input id="check" type="checkbox" class="check" checked>
+                                        <label for="check"><span class="icon"></span> Lembrar Senha</label>
+                                    </div>
+                                    <div class="group">
+                                        <input type="submit" class="button" value="Entrar">
+                                    </div>
+                                    <div class="hr"></div>
+                                    <div class="foot-lnk">
+                                        <label for="tab-2">Recuperar Senha?</label>
+                                    </div>
+                                </div>
+                                <div class="for-pwd-htm">
+                                    <div class="group">
+                                        <label for="user" class="label">Usuário</label>
+                                        <input id="user" type="text" class="input">
+                                    </div>
+                                    <div class="group">
+                                        <input type="submit" class="button" value="Recuperar Senha">
+                                    </div>
+                                    <div class="hr"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
 </body>
 </html>
 
