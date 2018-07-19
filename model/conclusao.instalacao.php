7a68092d89bcf304c7c685477819af2ea6c9170b
Italo <italomoraesalmeida@hotmail.com>
2018-07-18 21:33:25 -0300

    Implementação da criação do usuário administrador

---
+++ model/conclusao.instalacao.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "../dao/class.insert.php";
+
+$nomAdm = $_POST['nomAdm'];
+$sobAdm = $_POST['sobAdm'];
+$emaAdm = $_POST['emaAdm'];
+$logAdm = $_POST['logAdm'];
+$pasAdm = $_POST['pasAdm'];
+
+$Insert = new Insert('tUsuario','',"'".$nomAdm."','".$sobAdm."','".$emaAdm."','".$logAdm."','".md5($pasAdm)."','00000000000','".date('Y-m-d H:i:s')."',00000000000,'I','".date('Y-m-d H:i:s')."',Null,1,1,1,1,1");
+
+$array = $Insert -> getInsert();
+
+if($array['val'] == 1){
+    
+}else{
+    echo '  <div class="alert alert-danger">'
+                .$array['msg'].
+            '</div>';
+}
--- view/form.install.adm.php
+++ view/form.install.adm.php
@@ -1,3 +1,112 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SOSTcket - Instalação</title>
 
-echo "OK";
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../resources/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../resources/css/index.css">
+    <link rel="stylesheet" type="text/css" href="../resources/css/aba.css">
+    <link rel="stylesheet" type="text/css" href="../resources/css/cssCheckbox.css">
+                                    
+    <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="../resources/css/fonts.css">
+
+</head>
+<body>
+
+    <!-- BANNER -->
+    <div class="banner col-md-12">
+        <h1 class="texto-banner">SOSTicket - Instalação</h1>
+    </div>
+
+    <!-- MASCARA PARA COBRIR O SITE -->
+    <div id="mascara"></div>
+    <!-- FIM DA MASCARA -->
+
+    <!-- TELA DE INSTALAÇÃO -->
+
+    <!-- CONTAINER DE INSTALAÇÃO -->
+    <div class="container">
+
+        <!-- JUMBOTRON -->
+        <div class="jumbotron index">
+
+            <!-- ALERTAS -->
+
+            <div id="retornos"></div>
+
+            <!-- FIM ALERTAS -->
+
+            <!-- Linha -->
+            <div class="row">
+
+                <!-- MENU TABELADO DE CADASTRO -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel with-nav-tabs panel-primary">
+                                <div class="panel-heading">
+                                    <ul class="nav nav-tabs">
+                                        <li class="active"><a href="#tabUsuAdm" data-toggle="tab">Administrador</a></li>
+                                    </ul>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="tab-content">
+                                        <div class="tab-pane fade in active" id="tabUsuAdm">
+                                            <!-- COL ADMINISTRADOR -->
+                                                <div class="col-md-12">
+                                                <fieldset>
+                                                    <legend>Cadastro do Administrador</legend>
+                                                        <form class="form-horizontal" data-toggle="validator" onsubmit="return false"> <!-- FORMULÁRIO -->
+                                                            <label>Nome</label>
+                                                            <input class="form-control" type="text" placeholder="Digite o Nome do Usuário Administrador" id="nomAdm" name="nomAdm" ><br>
+                                                            <label>Sobrenome</label>
+                                                            <input class="form-control" type="text" placeholder="Digite o Sobrenome do Usuário Administrador" id="sobAdm" name="sobAdm" ><br>
+                                                            <label>Email</label>
+                                                            <input class="form-control" type="email" placeholder="Digite o Email do Usuário Administrador" id="emaAdm" name="emaAdm" ><br>
+                                                            <label>Login</label>
+                                                            <input class="form-control" type="text" placeholder="Digite o Login do Usuário Administrador" id="logAdm" name="logAdm" ><br>
+                                                            <label>Senha</label>
+                                                            <input class="form-control" type="password" placeholder="Digite a Senha do Usuário Administrador" id="pasAdm" name="pasAdm" data-minlength="1" ><br>
+                                                </fieldset>
+                                                
+                                                <!-- CONCLUSÇÃO INSTALAÇÃO -->
+                                                <fieldset>
+                                                    <div>
+                                                        <input type="button" class="btn btn-inverse btn-site" name="btnUsuAdm" id="btnUsuAdm" onclick="ajaxPost('../model/conclusao.instalacao.php', '#retornos')" value="Concluir">
+                                                    </div>
+                                                </fieldset>
+                                                <!-- FIM CONCLUSÃO INSTALAÇÃO -->
+                                            </div>
+                                            <!-- FIM COL ADMINISTRADOR -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- FIM MENU TABELADO -->
+
+            </div>
+            <!-- FIM LINHA -->
+
+            <div></div>
+
+        </div>
+        <!-- FIM JUMBOTRON -->
+
+    </div>
+    <!-- FIM DO CONTAINER DE INSTALAÇÃO -->
+
+    <!--JavaScript-->
+    <script type="text/javascript" src="../resources/js/jquery.js"></script>
+    <script type="text/javascript" src="../resources/js/post.js"></script> 
+    <script type="text/javascript" src="../resources/js/bootstrap.js"></script>
+    <script type="text/javascript" src="../resources/js/index.js"></script>
+    
+</body>
+</html>
